358487e7eabcfa2bbc40c41907437a0926a7dcc7
chvaldez10 <christianvaldez021@gmail.com>
2024-05-29 18:47:17 -0600

    added static and media url

--- k-ecomm-server/k_ecomm_server/settings.py
+++ k-ecomm-server/k_ecomm_server/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR, "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -139,7 +144,6 @@ JAZZMIN_SETTINGS = {
     "welcome_sign": "Welcome",
     "show_sidebar": True,
     "show_ui_builder": True,
-    # Copyright on the footer
     "copyright": "chvaldez10",
 }
 
