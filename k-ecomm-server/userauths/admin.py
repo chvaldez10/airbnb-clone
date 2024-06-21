964a4063949575f2204c4a6fd0edbfc115a3778a
chvaldez10 <christianvaldez021@gmail.com>
2024-06-20 22:37:26 -0600

    added token pair

--- k-ecomm-server/userauths/admin.py
+++ k-ecomm-server/userauths/admin.py
@@ -5,7 +5,7 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["full_name", "email", "username"]
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ["full_name", "address", "user"]
+    list_display = ["full_name", "address", "user", "pid", "date"]
     search_fields = ["full_name"]
 
 admin.site.register(User, UserAdmin)
--- k-ecomm-server/userauths/serializer.py
+++ k-ecomm-server/userauths/serializer.py
@@ -1,6 +1,51 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from userauths.models import Profile, User
+from django.contrib.auth.password_validation import validate_password
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token["full_name"] = user.full_name
+        token["email"] = user.email
+        token["username"] = user.username
+
+        try:
+            token["vendor_id"] = user.vendor_id
+        except:
+            token["vendor_id"] = 0
+
+        return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ("full_name", "email", "phone", "password", "password2")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "Password did not match."})
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            full_name=validated_data["full_name"],
+            email=validated_data["email"],
+            phone=validated_data["phone"]
+        )
+
+        email_username, mobile = user.email.split("@")
+        user.username = email_username
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -15,3 +60,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         response = super().to_representation(instance)
         response["user"] = UserSerializer(instance.user).data
         return response
+    
+# class PasswordResetsSerializer(serializers.Serializer):
+#     email = serializers.EmailField()
